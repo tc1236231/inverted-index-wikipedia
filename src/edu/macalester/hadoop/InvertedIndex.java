093c038179d2c568fe7a719a1bd957e5a84f0d53
fenmaz <tnguyen9@macalester.edu>
2019-04-16 16:50:33 -0500

    write run function for driver

--- src/edu/macalester/hadoop/InvertedIndex.java
+++ src/edu/macalester/hadoop/InvertedIndex.java
@@ -1,25 +1,63 @@
 package edu.macalester.hadoop;
 
-import org.apache.log4j.Logger;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-import java.io.IOException;
-
-public class InvertedIndex {
-
-    private static final transient Logger logger = Logger.getLogger("app");
+public class InvertedIndex extends Configured implements Tool {
 
 
-    public static void main(String[] args) {
-        logger.debug("main");
-        try {
-            run(args[0], args[1]);
-        } catch (IOException io) {
-            logger.fatal("IO Exception " + InvertedIndex.class.getName() + " " + io);
-        }
-
+    public static void main(String[] args) throws Exception{
+        System.exit(ToolRunner.run(new Configuration(), new InvertedIndex(), args));
     }
 
-    public static void run(String input, String output) throws IOException {
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set(XmlInputFormat.START_TAG_KEY, "<page>");
+        conf.set(XmlInputFormat.END_TAG_KEY, "</page>");
 
+        if (args.length < 2) {
+            System.err.println("Usage: invertedindex <input_folder> <output_folder>");
+            return 2;
+        }
+
+        Job job = Job.getInstance(getConf());
+
+        job.setJarByClass(InvertedIndex.class);
+
+        job.setMapperClass(InvertedIndexMapper.class);
+        job.setReducerClass(InvertedIndexReducer.class);
+
+        job.setInputFormatClass(XmlInputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+
+        FileInputFormat.setInputPaths(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        FileSystem fs = FileSystem.newInstance(getConf());
+
+        if (fs.exists(outputPath)) {
+            fs.delete(outputPath, true);
+        }
+
+        if (job.waitForCompletion(true)) {
+            return 0;
+        } else return 1;
     }
 }
