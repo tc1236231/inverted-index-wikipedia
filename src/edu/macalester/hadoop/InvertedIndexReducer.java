bf72d24c5979c892b500068a7e052a7d6046c8c3
fenmaz <tnguyen9@macalester.edu>
2019-04-16 16:03:27 -0500

    Write the reduce function

--- src/edu/macalester/hadoop/InvertedIndexReducer.java
+++ src/edu/macalester/hadoop/InvertedIndexReducer.java
@@ -4,11 +4,20 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class InvertedIndexReducer extends Reducer<Text, Text, Text, Text> {
 
     @Override
-    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    public void reduce(Text word, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        HashMap<String, Integer> fileFreq = new HashMap<>();
 
+        for (Text page: values) {
+            String pageString = page.toString();
+            Integer count = fileFreq.getOrDefault(pageString, 0);
+            fileFreq.put(pageString, count + 1);
+        }
+        context.write(word, new Text(fileFreq.toString()));
     }
 }
