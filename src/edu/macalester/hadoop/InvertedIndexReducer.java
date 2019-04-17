befa9e0d0d3d12d2bf5c35ecad5dfc740cd16930
fenmaz <tnguyen9@macalester.edu>
2019-04-17 00:11:23 -0500

    logging

--- src/edu/macalester/hadoop/InvertedIndexReducer.java
+++ src/edu/macalester/hadoop/InvertedIndexReducer.java
@@ -2,15 +2,19 @@ package edu.macalester.hadoop;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+//import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.HashMap;
 
 public class InvertedIndexReducer extends Reducer<Text, Text, Text, Text> {
 
+    //private static final Logger logger = Logger.getLogger(InvertedIndexReducer.class);
+
     @Override
     public void reduce(Text word, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         HashMap<String, Integer> fileFreq = new HashMap<>();
+        //logger.debug("word = " + word.toString());
 
         for (Text page: values) {
             String pageString = page.toString();
