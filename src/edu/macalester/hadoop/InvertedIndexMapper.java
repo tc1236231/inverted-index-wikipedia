e9e4903ef6ecf2a29b30772576aff5ae9ac0b8da
fenmaz <tnguyen9@macalester.edu>
2019-04-17 12:27:37 -0500

    parse wiki text with WikiClean

--- src/edu/macalester/hadoop/InvertedIndexMapper.java
+++ src/edu/macalester/hadoop/InvertedIndexMapper.java
@@ -3,10 +3,10 @@ package edu.macalester.hadoop;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.wikiclean.WikiClean;
 //import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.nio.charset.CharacterCodingException;
 import java.util.StringTokenizer;
 
 
@@ -14,62 +14,44 @@ public class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text> 
 
     //private static final transient Logger logger = Logger.getLogger("app");
     private Text word = new Text();
+    private WikiClean cleaner = new WikiClean.Builder()
+            .withFooter(false)
+            .withLanguage(WikiClean.WikiLanguage.EN)
+            .withTitle(false)
+            .build();
 
 
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-        // Returns  String[0] = <title>[TITLE]</title>
-        //          String[1] = <text>[CONTENT]</text>
-        // !! without the <tags>.
         String[] titleAndText = parseTitleAndText(value);
 
         String pageString = titleAndText[0];
-        // TODO: parse the text to get actual meaningful words.
         String text = titleAndText[1].toLowerCase();
 
         if(notValidPage(pageString))
             return;
 
         Text page = new Text(pageString.replace(' ', '_'));
-        StringTokenizer tokenizer = new StringTokenizer(text, " \t\n\r\f\",.:;?![](){}*'");
+        StringTokenizer tokenizer = new StringTokenizer(text, " \t\n\r\f\",.:;?!#[](){}*");
 
         while (tokenizer.hasMoreTokens()) {
             String wordText = tokenizer.nextToken();
-
-            // Detecting and excluding stop words should be done here
-            //if (stopwords.contains(word)) continue;
-
             word.set(wordText);
             context.write(word, page);
-
         }
-
     }
 
     private boolean notValidPage(String pageString) {
         return pageString.contains(":");
     }
 
-    private String[] parseTitleAndText(Text value) throws CharacterCodingException {
+    private String[] parseTitleAndText(Text value) {
         String[] titleAndText = new String[2];
 
-        int start = value.find("<title>");
-        int end = value.find("</title>", start);
-        start += 7; //add <title> length.
-
-        titleAndText[0] = Text.decode(value.getBytes(), start, end-start);
-
-        start = value.find("<text");
-        start = value.find(">", start);
-        end = value.find("</text>", start);
-        start += 1;
-
-        if(start == -1 || end == -1) {
-            return new String[]{"",""};
-        }
-
-        titleAndText[1] = Text.decode(value.getBytes(), start, end-start);
+        String valueStr = value.toString();
+        titleAndText[0] = cleaner.getTitle(valueStr);
+        titleAndText[1] = cleaner.clean(valueStr);
 
         return titleAndText;
     }
